81d69ecbd4d9ed8f684ba20e8224a36335d1ed9c
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-08-28 00:37:14 +0200

    Update SignInForm.js

--- src/components/signInForm/SignInForm.js
+++ src/components/signInForm/SignInForm.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 async function loginUser(credentials) {
   return fetch('https://mecadom.electroniqueclasse.com/api/login', {
     method: 'POST',
@@ -9,66 +10,65 @@ async function loginUser(credentials) {
     body: JSON.stringify(credentials)
   })
     .then(data => data.json())
- }
+}
 
- 
- 
- 
-function SignInForm({toggle}) {
+function SignInForm({ toggle }) {
 
   const [useremail, setUserEmail] = useState();
   const [password, setPassword] = useState();
 
-    const handleSubmit = async e => {
+  const handleSubmit = async e => {
     e.preventDefault();
     const response = await loginUser({
       useremail,
       password
     });
     if ('accessToken' in response) {
-      return("Success", response.message, "success", {
+      // Remplacez cette fonction par la fonction appropriée pour afficher un message à l'utilisateur
+      displayMessage("Success", response.message, "success", {
         buttons: false,
         timer: 2000,
       })
-      .then((value) => {
-        localStorage.setItem('accessToken', response['accessToken']);
-        localStorage.setItem('user', JSON.stringify(response['user']));
-        window.location.href = "/profile";
-      });
+        .then((value) => {
+          localStorage.setItem('accessToken', response['accessToken']);
+          localStorage.setItem('user', JSON.stringify(response['user']));
+          window.location.href = "/profile";
+        });
     } else {
-        return("Failed", response.message, "error");
+      // Remplacez cette fonction par la fonction appropriée pour afficher un message à l'utilisateur
+      displayMessage("Failed", response.message, "error");
     }
   }
-    return (
-        <form className='login-form' noValidate onSubmit={handleSubmit}>
-            <h1 className='text-2xl font-bold'>Connexion</h1>
 
-            <div class="login-sign-up">
-                <div class="login" id="login">
-                    <input type="email" name="email" 
-                        placeholder="Email" required onChange={e => setUserEmail(e.target.value)} />
+  return (
+    <form className='login-form' noValidate onSubmit={handleSubmit}>
+      <h1 className='text-2xl font-bold'>Connexion</h1>
 
-                    <div class="password-field">
+      <div className="login-sign-up">
+        <div className="login" id="login">
+          <input type="email" name="email"
+            placeholder="Email" required onChange={e => setUserEmail(e.target.value)} />
 
-                        <input type="password"  placeholder="Mot de passe" name="password"
-                             required onChange={e => setPassword(e.target.value)} />
-                        <div class="show-password">
-                            <input type="checkbox" onclick="showPassword()" />
-                            <span>Voir</span>
-                        </div>
-                    </div>
-                </div>
+          <div className="password-field">
 
-
-                <div class="link-connexion" id="linkLogin">
-                    <Link href="#">Mot de passe oublié ?</Link>
-                    <span onClick={toggle}>S'enregistrer</span>
-                </div>
+            <input type="password" placeholder="Mot de passe" name="password"
+              required onChange={e => setPassword(e.target.value)} />
+            <div className="show-password">
+              <input type="checkbox" onClick={showPassword} />
+              <span>Voir</span>
             </div>
+          </div>
+        </div>
 
-            <input type="submit" value="Se connecter" id="loginBtn" />
-        </form>
-    )
+
+        <div className="link-connexion" id="linkLogin">
+          <Link to="#">Mot de passe oublié ?</Link>
+          <span onClick={toggle}>S'enregistrer</span>
+        </div>
+      </div>
+
+      <input type="submit" value="Se connecter" id="loginBtn" />
+    </form>
+  )
 }
-
-export default SignInForm
+export default SignInForm;
