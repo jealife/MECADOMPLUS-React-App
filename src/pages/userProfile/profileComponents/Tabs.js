ef2d9325094ac8859f4eb4cf797f4513fb743d8d
JEaLiFe jeaguy <jeanguylanememiaghe@gmail.com>
2023-08-28 12:21:35 +0000

    modified:   src/pages/userProfile/profileComponents/Tabs.js

--- src/pages/userProfile/profileComponents/Tabs.js
+++ src/pages/userProfile/profileComponents/Tabs.js
@@ -70,7 +70,7 @@ const Tabs = () => {
               </a>
             </li>
           </ul>
-          <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+          <div className="relative flex flex-col min-w-0 break-words px-3 bg-white w-full mb-6 shadow-lg rounded">
             <div className="px-4 py-5 flex-auto">
               <div className="tab-content tab-space">
                 <div className={openTab === 1 ? "block" : "hidden"} id="link1">
