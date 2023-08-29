10046dcf548edf7019d11b67e71c3b6dd95e2bde
JEaLiFe jeaguy <jeanguylanememiaghe@gmail.com>
2023-08-29 03:04:37 +0000

    modified:   src/pages/userProfile/profileComponents/tabsItems/ProfilTab.js

--- src/pages/userProfile/profileComponents/tabsItems/ProfilTab.js
+++ src/pages/userProfile/profileComponents/tabsItems/ProfilTab.js
@@ -4,7 +4,7 @@ export default function ProfilTab() {
     return (
         <div>
             <div class="mb-3 pt-0">
-                <input type="text" placeholder="Small Input" class="px-2 py-1 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:shadow-outline w-full" />
+                <input type="text" placeholder="Small Input" class="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:shadow-outline w-full" />
             </div>
             <div class="mb-3 pt-0">
                 <input type="text" placeholder="Regular Input" class="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:shadow-outline w-full" />
